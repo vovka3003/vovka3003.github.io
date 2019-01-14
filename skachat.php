3808a6af7c0be310f6f8a7287ca369f5c3e6effd
vovka3003 <vovka3003@ya.ru>
2019-01-14 11:06:21 +0300

    1

--- skachat.php
+++ skachat.php
@@ -6,7 +6,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="assets/css/style.css" rel="stylesheet">
-  <link rel="icon" type="image/png" href="favicon.png">
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
