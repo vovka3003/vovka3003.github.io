f63f52f3705274627b8bfbf7370cb2f6a9e5141b
Владимир <vovka3003@ya.ru>
2019-05-03 18:19:02 +0300

    Update index.js

--- test/index.js
+++ test/index.js
@@ -10,5 +10,5 @@ server.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err);
     }
-    console.log(`server is listening on ${port}`);
+    console.log('server is listening on ${port}');
 });
