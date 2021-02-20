630e43fc5158b1ca60e67cc00bf8659c5c1c0efd
vovka3003 <vovka3003@ya.ru>
2021-02-20 16:25:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 function waitUntilElementPresent(tagName, timeoutInSeconds) {
+	
+	
+	
     var currentTime = new Date().getTime();
 	
     var endTime = currentTime + timeoutInSeconds * 1000;
     var checkExist = setInterval(function () {
+		
+		
+		
 		var sections = document.getElementsByTagName('section');
         if (sections[sections.length-1].innerText.indexOf('with Mobirise')) {
             clearInterval(checkExist);
+			
 			var sections = document.getElementsByTagName('section');
 			sections[sections.length-1].innerHTML = '';
-			//console.log('clear');
+			
+		    document.getElementById('content4-r').childNodes[1].innerHTML = '<h1>Привет</h1>';
+
             return;
+			
         } else if (endTime < new Date().getTime()) {
             clearInterval(checkExist);
             console.log('not found in specified time.');
