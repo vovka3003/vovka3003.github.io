494c71db2224c373a8675c81e40392f1c28ab097
vovka3003 <vovka3003@ya.ru>
2021-02-22 21:09:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function(){ 
 		
  var kotletopriemnik = document.getElementById('content4-r').childNodes[1];
-			if  (kotletopriemnik is not null) { kotletopriemnik.innerHTML = '<iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&targets=%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%BA%D0%B0%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0%20Develop-Soft&targets-hint=&default-sum=&button-text=13&payment-type-choice=on&mobile-payment-type-choice=on&comment=on&hint=%D0%A2%D1%83%D1%82%20%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE%20%D0%BD%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C%20%D1%81%D0%B2%D0%BE%D0%B9%20%D0%BA%D0%BE%D0%BC%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%80%D0%B8%D0%B9&successURL=https%3A%2F%2Fvovka3003.github.io%2FOn_Money_Insert.html%23donate_success&quickpay=shop&account=4100185639160" width="100%" height="316" frameborder="0" allowtransparency="true" scrolling="no"></iframe>'; }
+			if  (kotletopriemnik is (not) null) { kotletopriemnik.innerHTML = '<iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&targets=%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%BA%D0%B0%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0%20Develop-Soft&targets-hint=&default-sum=&button-text=13&payment-type-choice=on&mobile-payment-type-choice=on&comment=on&hint=%D0%A2%D1%83%D1%82%20%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE%20%D0%BD%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C%20%D1%81%D0%B2%D0%BE%D0%B9%20%D0%BA%D0%BE%D0%BC%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%80%D0%B8%D0%B9&successURL=https%3A%2F%2Fvovka3003.github.io%2FOn_Money_Insert.html%23donate_success&quickpay=shop&account=4100185639160" width="100%" height="316" frameborder="0" allowtransparency="true" scrolling="no"></iframe>'; }
 			
 			
 var sections = document.getElementsByTagName('section');
